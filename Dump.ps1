6b8367cf44e5005b04f12bbc2938f7d1602f623e
Fischer <kevin.fischer@weidmueller.com>
2023-11-06 09:50:59 +0100

    Finished Dump.ps1

--- Dump.ps1
+++ Dump.ps1
@@ -1,13 +1,15 @@
 $output = @()
-$vulnConnectors = @("connector_base", "excelonlinebusiness", "github", "gmail", "keyvault", "shared_azureblob", "shared_azurequeues", "shared_azuretables", "shared_documentdb", "shared_sql")
+$vulnConnectors = @("connector_base", "excelonlinebusiness","shared_github" ,"github", "gmail", "keyvault", "shared_azureblob", "shared_azurequeues", "shared_azuretables", "shared_documentdb", "shared_sql")
 foreach($app in Get-AdminPowerApp){
     $owner = $app.Owner.displayName
     $appName = $app.AppName
     $appDisplayName = $app.DisplayName
     $envName = $app.EnvironmentName
     $vulnerable = $false
+    $sharedWith = @()
     foreach($principal in (Get-AdminPowerAppRoleAssignment -AppName $appName -EnvironmentName $envName)){
-        if($principal.PrincipalDisplayName -eq "Jeder"){
+        $sharedWith += $principal.PrincipalDisplayName
+        if(($principal.PrincipalDisplayName -eq "Jeder") -or ($principal.PrincipalDisplayName -eq "Everyone")){
             $vulnerable = $true
             break
         }
@@ -17,9 +19,19 @@ foreach($app in Get-AdminPowerApp){
     }
     $connectors = @()
     foreach($connector in (Get-AdminPowerAppConnection -EnvironmentName $app.EnvironmentName)){
+        Write-Host $connector.ConnectorName
         if($connector.ConnectorName -in $vulnConnectors){
-            $connectors += $connector
+            $connectors += $connector.ConnectorName
         }
     }
-    Write-Host $connectors
+    $output += @{
+        AppDisplayName = $appDisplayName
+        AppName = $appName
+        EnvironmentName = $envName
+        Owner = $owner
+        SharedWith = $sharedWith
+        Connections = $connectors
+    }
 }
+$output = $output | ConvertTo-Json -Compress | ConvertFrom-Json
+$output | Format-Table -AutoSize | Out-File o.txt

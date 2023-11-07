96cac1403e7e1b3e75fadead7895f2507421f9b7
Fischer <kevin.fischer@weidmueller.com>
2023-11-07 11:22:04 +0100

    Added iteration through all environments.

--- Dump.ps1
+++ Dump.ps1
@@ -1,34 +1,29 @@
 $output = @()
 $vulnConnectors = @("shared_excelonlinebusiness","shared_github", "shared_gmail", "shared_keyvault", "shared_azureblob", "shared_azurequeues", "shared_azuretables", "shared_documentdb", "shared_sql")
-foreach($app in Get-AdminPowerApp){
-    $owner = $app.Owner.displayName
-    $appName = $app.AppName
-    $appDisplayName = $app.DisplayName
-    $envName = $app.EnvironmentName
-    $vulnerable = $false
-    $sharedWith = @()
-    foreach($principal in (Get-AdminPowerAppRoleAssignment -AppName $appName -EnvironmentName $envName)){
-        $sharedWith += $principal.PrincipalDisplayName
-        if(($principal.PrincipalType -eq "Tenant")){
-            $vulnerable = $true
-            break
+foreach($env in Get-AdminPowerAppEnvironment){
+    foreach($app in Get-AdminPowerApp -EnvironmentName $env.EnvironmentName){
+        $vulnerable = $false
+        foreach($principal in (Get-AdminPowerAppRoleAssignment -AppName $app.AppName -EnvironmentName $app.EnvironmentName)){
+            if(($principal.PrincipalType -eq "Tenant")){
+                $vulnerable = $true
+                break
+            }
         }
-    }
-    if(!$vulnerable){
-        continue
-    }
-    $connectors = @()
-    foreach($connector in (Get-AdminPowerAppConnection -EnvironmentName $app.EnvironmentName)){
-        if($connector.ConnectorName -in $vulnConnectors){
-            $connectors += $connector.ConnectorName
+        if(!$vulnerable){continue}
+        $connectors = @()
+        foreach($connector in (Get-AdminPowerAppConnection -EnvironmentName $app.EnvironmentName)){
+            if($connector.ConnectorName -in $vulnConnectors){
+                $connectors += $connector.ConnectorName
+            }
         }
-    }
-    $output += @{
-        AppDisplayName = $appDisplayName
-        AppName = $appName
-        EnvironmentName = $envName
-        Owner = $owner
-        Connections = $connectors
+        if($connectors.Count -eq 0){continue}
+        $output += @{
+            AppDisplayName = $app.DisplayName
+            AppName = $app.AppName
+            EnvironmentName = $app.EnvironmentName
+            Owner = $app.Owner.displayName
+            Connections = $connectors
+        }
     }
 }
 $output = $output | ConvertTo-Json -Compress | ConvertFrom-Json
